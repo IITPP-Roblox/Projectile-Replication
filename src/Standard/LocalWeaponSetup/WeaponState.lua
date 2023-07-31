1f4d8feee7b5b1842505ae86fd9ea9ad8727797c
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-07-31 01:38:23 -0400

    Move weapon firing logic to separate module.

---
+++ src/Standard/LocalWeaponSetup/WeaponState.lua
@@ -0,0 +1,132 @@
+--[[
+TheNexusAvenger
+
+Manages the weapon state for firing and reloading.
+--]]
+--!strict
+
+local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
+
+local BaseInput = require(script.Parent:WaitForChild("Input"):WaitForChild("BaseInput"))
+local Types = require(script.Parent.Parent.Parent:WaitForChild("Types"))
+
+local WeaponState = {}
+WeaponState.__index = WeaponState
+
+export type WeaponState = {
+    Firing: boolean,
+    LastFireTime: number,
+    Input: BaseInput.BaseInput,
+    Configuration: Types.StandardConfiguration,
+    Tool: Instance,
+    Handle: BasePart,
+    StartAttachment: Attachment,
+    ReloadingValue: BoolValue,
+    RemainingRoundsValue: IntValue,
+    ChargedPercentValue: NumberValue?,
+    ProjectileReplication: any,
+
+    new: (Tool: Instance, Input: BaseInput.BaseInput) -> (WeaponState),
+    GetAim: (self: WeaponState) -> (Vector3),
+    Reload: (self: WeaponState) -> (),
+    TryFire: (self: WeaponState) -> (),
+    Fire: (self: WeaponState) -> (),
+    StopFiring: (self: WeaponState) -> (),
+}
+
+
+
+--[[
+Creates a weapon state.
+--]]
+function WeaponState.new(Tool: Instance, Input: BaseInput.BaseInput): WeaponState
+    local Handle = Tool:WaitForChild("Handle") :: BasePart
+    local State = Tool:WaitForChild("State")
+    return (setmetatable({
+        Firing = false,
+        LastFireTime = 0,
+        Input = Input,
+        Configuration = require(Tool:WaitForChild("Configuration")),
+        Tool = Tool,
+        Handle = Handle,
+        StartAttachment = Handle:WaitForChild("StartAttachment") :: Attachment,
+        ReloadingValue = State:WaitForChild("Reloading"),
+        RemainingRoundsValue = State:WaitForChild("RemainingRounds"),
+        ChargedPercentValue = State:FindFirstChild("ChargedPercent") :: NumberValue,
+        --ProjectileReplication is stored internally to prevent a cyclic require.
+        ProjectileReplication = require(script.Parent.Parent.Parent) :: any,
+    }, WeaponState) :: any) :: WeaponState
+end
+
+--[[
+Returns the current aim position.
+--]]
+function WeaponState:GetAim(): Vector3
+    if UserInputService.VREnabled then
+        return ((self.StartAttachment :: Attachment).WorldCFrame * CFrame.new(0, 0, -10000)).Position
+    end
+    return self.Input:GetTargetWorldSpace()
+end
+
+--[[
+Tries to reload the weapon.
+--]]
+function WeaponState:Reload(): ()
+    if self.ReloadingValue.Value then return end
+    self.ReloadingValue.Value = true
+    self.ProjectileReplication:Reload(Players.LocalPlayer, self.Tool)
+end
+
+--[[
+Tries to fire the weapon once.
+--]]
+function WeaponState:TryFire(): ()
+    --Return if there are no rounds or the last fire was too recent.
+    if self.ReloadingValue.Value then return end
+    if self.RemainingRoundsValue.Value <= 0 then
+        self:Reload()
+        return
+    end
+    if tick() - self.LastFireTime < self.Configuration.CooldownTime then return end
+
+    --Fire the weapon.
+    self.LastFireTime = tick()
+    self.RemainingRoundsValue.Value = self.RemainingRoundsValue.Value - 1
+    for _ = 1, self.Configuration.ProjectilesPerRound or 1 do
+        self.ProjectileReplication:Fire(CFrame.new(self.StartAttachment.WorldPosition, self:GetAim()) * CFrame.Angles(0, 0, math.random() * math.pi * 2) * CFrame.Angles(math.random() * self.Configuration.ProjectileSpread, 0, 0), self.Handle, self.Configuration.ProjectilePreset)
+    end
+end
+
+--[[
+Fires the weapon.
+For automatic weapons, this method will run until the firing is stopped.
+--]]
+function WeaponState:Fire(): ()
+    self.Firing = true
+    if self.Configuration.FullAutomatic then
+        if self.ChargedPercentValue then
+            while self.ChargedPercentValue.Value < 1 do
+                task.wait()
+            end
+        end
+        while self.Firing and (not self.ChargedPercentValue or self.ChargedPercentValue.Value >= 1) do
+            self:TryFire()
+            task.wait(self.Configuration.CooldownTime)
+        end
+    else
+        self:TryFire()
+    end
+    self.Firing = false
+end
+
+--[[
+Stops firing the weapon, if it is being fired.
+--]]
+function WeaponState:StopFiring(): ()
+    self.Firing = false
+end
+
+
+
+return (WeaponState :: any) :: WeaponState
--- src/Standard/LocalWeaponSetup/init.lua
+++ src/Standard/LocalWeaponSetup/init.lua
@@ -10,6 +10,7 @@ local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 
+local WeaponState = require(script:WaitForChild("WeaponState"))
 local CombinedInput = require(script:WaitForChild("Input"):WaitForChild("CombinedInput"))
 local GamepadInput = require(script:WaitForChild("Input"):WaitForChild("GamepadInput"))
 local MouseInput = require(script:WaitForChild("Input"):WaitForChild("MouseInput"))
@@ -26,76 +27,18 @@ local LocalWeaponSetup = {}
 Sets up a tool.
 --]]
 function LocalWeaponSetup:SetupTool(Tool: Tool): ()
-    local ProjectileReplicationModule = script.Parent.Parent
-    local ProjectileReplication = require(ProjectileReplicationModule) :: any
+    local ProjectileReplication = require(script.Parent.Parent) :: any
     
     local Handle = Tool:WaitForChild("Handle")
     local StartAttachment = Handle:WaitForChild("StartAttachment") :: Attachment
-    local Configuration = require(Tool:WaitForChild("Configuration")) :: any
     local Input = CombinedInput.new(MouseInput.new(), TouchInput.new(), GamepadInput.new(Enum.KeyCode.ButtonR2))
     Input:ConnectReloadButton(Enum.KeyCode.ButtonY)
 
-    local State = Tool:WaitForChild("State")
-    local ChargedPercentValue = State:FindFirstChild("ChargedPercent") :: NumberValue
-    local RemainingRounds = State:WaitForChild("RemainingRounds") :: IntValue
-    local ReloadingValue = State:WaitForChild("Reloading") :: BoolValue
-
     local CurrentCrosshair: BaseCrosshair.BaseCrosshair? = nil
-    local LastFireTime = 0
     local Equipped = false
-    local Firing = false
-
-    --[[
-    Returns the current mouse position using the same raycasting
-    logic used by the projectiles.
-    --]]
-    local function GetMousePosition(): Vector3
-        if UserInputService.VREnabled then
-            return (StartAttachment.WorldCFrame * CFrame.new(0, 0, -10000)).Position
-        end
-        return Input:GetTargetWorldSpace()
-    end
-
-    --[[
-    Fires the weapon.
-    --]]
-    local function Fire(): ()
-        if not Equipped then return end
-        for _ = 1, Configuration.ProjectilesPerRound or 1 do
-            ProjectileReplication:Fire(CFrame.new(StartAttachment.WorldPosition, GetMousePosition()) * CFrame.Angles(0, 0, math.random() * math.pi * 2) * CFrame.Angles(math.random() * Configuration.ProjectileSpread, 0, 0), Handle, Configuration.ProjectilePreset)
-        end
-    end
-
-    --[[
-    Tries to reload the weapon.
-    --]]
-    local function TryReload(): ()
-        if ReloadingValue.Value then return end
-        ReloadingValue.Value = true
-        ProjectileReplication:Reload(Players.LocalPlayer, Tool)
-    end
-
-    --[[
-    Tries to fire the weapon.
-    --]]
-    local function TryFire(): ()
-        --Return if there are no rounds or the last fire was too recent.
-        if ReloadingValue.Value then return end
-        if RemainingRounds.Value <= 0 then
-            TryReload()
-            return
-        end
-        if tick() - LastFireTime < Configuration.CooldownTime then return end
-
-        --Fire the weapon.
-        LastFireTime = tick()
-        RemainingRounds.Value = RemainingRounds.Value - 1
-        Fire()
-    end
-
-
 
     --Connect equipping and unequipping the tool.
+    local WeaponState = WeaponState.new(Tool, Input)
     Tool.Equipped:Connect(function()
         Equipped = true
 
@@ -111,16 +54,15 @@ function LocalWeaponSetup:SetupTool(Tool: Tool): ()
 
             local CrosshairAsMouse = (Crosshair :: MouseCrosshair.MouseCrosshair)
             while Equipped do
-                local TargetPosition = Input:GetTargetScreenSpace()
-                CrosshairAsMouse:MoveTo(TargetPosition)
-                ProjectileReplication:Aim(Players.LocalPlayer, GetMousePosition())
+                CrosshairAsMouse:MoveTo(Input:GetTargetScreenSpace())
+                ProjectileReplication:Aim(Players.LocalPlayer, WeaponState:GetAim())
                 RunService.RenderStepped:Wait()
             end
         end
     end)
     Tool.Unequipped:Connect(function()
         Equipped = false
-        Firing = false
+        WeaponState:StopFiring()
 
         if CurrentCrosshair then
             CurrentCrosshair:Destroy()
@@ -131,25 +73,15 @@ function LocalWeaponSetup:SetupTool(Tool: Tool): ()
     --Connect using the tool.
     Input.StartFire:Connect(function()
         if not Equipped then return end
-        if Configuration.FullAutomatic then
-            Firing = true
-            if ChargedPercentValue then
-                while ChargedPercentValue.Value < 1 do
-                    task.wait()
-                end
-            end
-            while Firing and (not ChargedPercentValue or ChargedPercentValue.Value >= 1) do
-                TryFire()
-                task.wait(Configuration.CooldownTime)
-            end
-        else
-            TryFire()
-        end
+        WeaponState:Fire()
     end)
     Input.EndFire:Connect(function()
-        Firing = false
+        WeaponState:StopFiring()
     end)
-    Input.Reload:Connect(TryReload)
+    Input.Reload:Connect(function()
+        if not Equipped then return end
+        WeaponState:Reload()
+    end)
 end
 
 --[[
